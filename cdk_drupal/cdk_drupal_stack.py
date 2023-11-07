ba525fe16e9421685b9e422d7fce2e2f5c15ab43
Sabari Rajasekar <sabari.rajasekar@wildstyle-network.com>
2023-11-07 18:00:05 +0530

    Add EC2 cdk

--- cdk_drupal/cdk_drupal_stack.py
+++ cdk_drupal/cdk_drupal_stack.py
@@ -1,19 +1,63 @@
+import os.path
+from constructs import Construct
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_cloudwatch as cloudwatch,
+    aws_elasticloadbalancingv2 as elb,
+    aws_s3_assets as s3_assets,
+    aws_iam as iam,
+    App, Stack
 )
-from constructs import Construct
 
-class CdkDrupalStack(Stack):
+dirname = os.path.dirname(__file__)
+class EC2InstanceStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # VPC
+        vpc = ec2.Vpc(self, "VPC",
+            nat_gateways=0,
+            subnet_configuration=[ec2.SubnetConfiguration(name="public",subnet_type=ec2.SubnetType.PUBLIC)]
+            )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkDrupalQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # AMI
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+            )
+
+        # Instance Role and SSM Managed Policy
+        role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
+
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
+
+        # Instance
+        instance = ec2.Instance(self, "Instance",
+            instance_type=ec2.InstanceType("t3.nano"),
+            machine_image=amzn_linux,
+            vpc = vpc,
+            role = role
+            )
+
+        # Script in S3 as Asset
+        asset = s3_assets(self, "s3_assets", path=os.path.join(dirname, "configure.sh"))
+        local_path = instance.user_data.add_s3_download_command(
+            bucket=asset.bucket,
+            bucket_key=asset.s3_object_key
+        )
+
+        # Userdata executes script from S3
+        instance.user_data.add_execute_file_command(
+            file_path=local_path
+            )
+        asset.grant_read(instance.role)
+
+app = App()
+EC2InstanceStack(app, "ec2-instance")
+app.synth()
