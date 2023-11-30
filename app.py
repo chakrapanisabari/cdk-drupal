c11b65fc49f1db8d504c6efcc271f71dcc864242
Sabari Rajasekar <sabari.rajasekar@wildstyle-network.com>
2023-11-30 13:23:31 +0530

    add env

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ EC2InstanceStack(app, "EC2InstanceStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
@@ -33,7 +33,7 @@ VpcDemoStack(app, "VpcDemoStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION'))
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
